52a8d0fb44b2723b1700bff74ea0e029b017f4aa
ingjjbr <88737926+ingjjbr@users.noreply.github.com>
2021-11-21 16:43:15 -0500

    Ajuste de codigo y diseño de estilos

--- app/src/main/java/com/ancreandoideas/appdesdecero/ListPOIActivity.kt
+++ app/src/main/java/com/ancreandoideas/appdesdecero/ListPOIActivity.kt
@@ -37,27 +37,5 @@ class ListPOIActivity : AppCompatActivity() {
 
     }
 
-/*
-    private fun createMockPois():ArrayList<Poi>{
-        return  arrayListOf(
-            Poi(
-                name = "Rafting",
-                description = "Rafting Chibiriquete",
-                urlPicture = "https://i.ibb.co/fnFczcY/icon-aventura-rafting.jpg",
-                 rating = 4.0,
-            ),
-            Poi(
-                name = "Vuelo",
-                description = "Sobre volar la sierra de  Chibiriquete",
-                urlPicture= "https://es.seaicons.com/wp-content/uploads/2016/05/airplane-icon.png",
-                rating = 4.5
-            ),
-            Poi(
-                name = "Senderismo",
-                description = "Senderismo Chibiriquete",
-                urlPicture= "https://st.depositphotos.com/2065405/4445/i/950/depositphotos_44455309-stock-photo-icon-button-pictogram-canoeing.jpg",
-                rating = 4.0
-            ),
-        )
-    }*/
+
 }
